2d3f12abb6714e142867c0c1b27e10c6bec49443
Gaurav Kumar <grvkmr891@gmail.com>
2024-07-11 03:52:19 +0530

    c7

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,23 +1,47 @@
-
 import React from 'react';
 import { useWishlist } from '../contexts/WishlistContext';
+import { useCart } from '../contexts/CartContext';
+import './whishlist.css';
 
 const WishlistPage = () => {
-    const { wishlist } = useWishlist();
+    const { wishlist, addToWishlist } = useWishlist();
+    const { addToCart } = useCart();
+
+    const isInWishlist = (item) => wishlist && wishlist.some(wishlistItem => wishlistItem.id === item.id);
 
     return (
-        <div className="wishlist-page">
-            <h2>Your Wishlist</h2>
+        <div className="left-content2">
             {wishlist.length === 0 ? (
                 <p>Your wishlist is empty</p>
             ) : (
-                <ul>
-                    {wishlist.map(item => (
-                        <li key={item.id}>
-                            {item.name} - {item.price}
-                        </li>
-                    ))}
-                </ul>
+                wishlist.map((item, index) => (
+                    
+                    <div className="image-boxes2" key={index}>
+                       
+                        <div className="image-container2">
+                            <img src={item.image} alt={item.name} />
+                            <i
+                                className={`fa-${isInWishlist(item) ? 'solid' : 'regular'} fa-heart heart-icon`}
+                                onClick={() => addToWishlist(item)}
+                            ></i>
+                        </div>
+                        <div className='ress'>
+                            <div className='chassu1'>
+                                <p className="brand-name2">{item.brand}</p>
+                                <p style={{fontSize:'13px'}}>⭐⭐⭐⭐⭐</p>
+                                <a href="#" onClick={() => addToCart(item)}>
+                                    Add to cart
+                                </a>
+                            </div>
+                            <div className='chassu2'>
+                                <h3>{item.name}</h3>
+                            </div>
+                            <div className='chassu3'>
+                                <p className="price2">{item.price}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))
             )}
         </div>
     );
